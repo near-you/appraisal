741cb93ca5426890e8a135cc53861d3f8e00e221
andrii <andrii.romanyshyn@jetsoftpro.com>
2022-11-11 14:14:19 +0200

    fix minor bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/5376f3c62b.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
